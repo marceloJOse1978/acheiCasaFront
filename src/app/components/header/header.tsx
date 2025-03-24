e1e7766eb80ff8ba3b93925e6ee171830a0ca807
Manuel Cardoso <manuelcardosomasc@gmail.com>
2025-03-24 17:53:42 +0100

    header manipulation

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,33 +1,46 @@
-import Image from 'next/image';
-import { Menu, User, MapPin, <Search />} from "lucide-react";
+import Image from "next/image";
+import { Menu, User, MapPin, Search } from "lucide-react";
 
 export default function Header() {
-    return (
-      <header className="header">
-        <div className="header-content">
-          <div id="logo-area">
-            <img src="/logo.png" alt="Logo" />
-          </div>
-          <div id="option-area">
-            <div>
-                <button>Hospedagem</button>
-                <button>Vendas</button>
-            </div>
-            <div id='search-area'>
-            <MapPin />
-            <input type="text" placeholder="Buscar..." />
-            </div>
-          </div>
-          <div id="menu-area">
-            <div id="menu-icon">
-                <Menu />
-            </div>
-            <div id="user-area">
-                <User />
-            </div>
-          </div>
+  return (
+    <header className="header">
+      <div className="logo-area">
+        <Image src="/logo.png" alt="Logo" width={100} height={50} priority />
+      </div>
+
+      <div className="option-area">
+        <div>
+          <button className="btn-hospedagem">Hospedagem</button>
+          <button className="btn-vendas">Vendas</button>
         </div>
-      </header>
-    );
-  }
-  
+
+        <div className="search-area">
+          <MapPin className="icon" />
+          <div>
+            <p className="search-location">Luanda</p>
+            <p className="search-destination">Escolha o seu destino</p>
+          </div>
+          <form className="search-form">
+            <input
+              type="text"
+              placeholder="Buscar..."
+              className="search-input"
+            />
+            <button type="submit" aria-label="Buscar" className="search-button">
+              <Search />
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div className="menu-area">
+        <button className="menu-icon" aria-label="Abrir Menu">
+          <Menu />
+        </button>
+        <button className="user-icon" aria-label="Área do Usuário">
+          <User />
+        </button>
+      </div>
+    </header>
+  );
+}
