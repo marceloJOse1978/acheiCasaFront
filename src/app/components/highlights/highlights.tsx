165e96c93d443f2b433dbd7c471bcea31c157ec7
Manuel Cardoso <manuelcardosomasc@gmail.com>
2025-03-31 18:43:02 +0100

    hight

---
+++ src/app/components/highlights/highlights.tsx
@@ -0,0 +1,43 @@
+
+'use client'
+
+import React from 'react'
+import "./highlights.css"
+import casas from './obje';
+import { Star } from 'lucide-react';
+
+const Highlights = () => {
+      
+      return (
+        <div className="grid-container">
+            {casas.map((casa) => (
+                <div key={casa.id} className="">
+                    <figure className='image-thumb'>
+                        <a href="#">
+                            <img src={casa.image} alt={casa.title} className="" />
+                        </a>
+                    </figure>
+                  
+                    <div className="">
+                        <div className='about'>
+                             <h3 className="title"><a href="#">{casa.location}</a></h3>
+                            <div className="rating">
+                                <Star className="rating-text" width={10}/>
+                                <span className="rating-text">{casa.rating}</span>
+                            </div>
+                        </div>
+                        <div className='group'>
+                            <div>
+                            <p className="dimensions">{casa.dimensions}</p>
+                            <p className="date">{casa.date}</p>
+                            </div>
+                            <p className="price">{casa.price}</p>
+                        </div>                        
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Highlights
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../app/components/header/header';
 import Footer from '../app/components/footer/footer';
 import Filter from './components/filter/filter';
+import Highlights from './components/highlights/highlights';
 
 export default function Home() {
   return (
     <>
       <Header />
       <Filter />
+      <Highlights />
       exemplo
       <Footer />
     </>
